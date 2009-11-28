d9755d95c7cee3b186b967d853784777017d7aed
DanielVartanov <daniel.vartanov@gmail.com>
2009-11-29 00:05:49 +0600

    Scope#define_method

--- lib/scope.rb
+++ lib/scope.rb
@@ -1,1 +1,7 @@
-Scope = Class.new(Array)
+class Scope < Class.new(Array)
+	def self.define_scope(name, proc)
+		define_method name do
+			select { |element| proc.call(element) }
+		end
+	end
+end
--- spec/scope_spec.rb
+++ spec/scope_spec.rb
@@ -12,11 +12,25 @@ describe Scope do
 			@scope.should == [1, 2]
 		end
 	end
-	
 
 	describe "given a derivative from Scope class" do
-		before :each do
+		before :all do
 			Numbers = Class.new(Scope)
-		end 
+		end
+
+		describe "when #define_scope is called with a proc" do
+			before :all do
+				class Numbers
+					define_scope :even, proc { |number| number % 2 == 0 }
+					define_scope :odd, proc { |number| number % 2 != 0 }
+				end
+			end
+
+			it "should return scoped elements according to given procs" do
+				@numbers = Numbers.new((1..6).to_a)
+				@numbers.even.should == [2, 4, 6]
+				@numbers.odd.should == [1, 3, 5]
+			end
+		end
 	end
 end
